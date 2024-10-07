c1a469b5e67a8693f54ffa52269c1966982609ba
dovekie <dovekie@gmail.com>
2024-10-07 14:15:42 -0700

    use JWT

--- libraryquest/urls.py
+++ libraryquest/urls.py
@@ -14,17 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from libraryquest_app import views
 
 router = DefaultRouter()
-router.register(r'libraries', views.LibraryView)
-router.register(r'readers', views.ReaderView)
+router.register(r"libraries", views.LibraryView)
+router.register(r"readers", views.ReaderView)
 
 urlpatterns = [
     path("", views.default_view),
     path("api/", include(router.urls)),
     path("admin/", admin.site.urls),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
 ]
--- libraryquest_ui/src/App.tsx
+++ libraryquest_ui/src/App.tsx
@@ -9,6 +9,9 @@ import { Header } from "./components/Header";
 function App() {
   const [libraries, setLibraries] = useState([] as ILibraryAddress[]);
   const [reader, setReader] = useState({} as IReader);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [token, setToken] = useState("");
 
   useEffect(() => {
     const getAllLibraries = async () => {
@@ -18,19 +21,36 @@ function App() {
     getAllLibraries();
   }, []);
 
-  useEffect(() => { // FIXME replace with a login
-    const getFirstReader = async () => {
-      const res = await axios.get("http://localhost:8000/api/readers/");
-      setReader(res.data[1]);
-    };
-    getFirstReader();
-  }, []);
+  const handleLogin = async () => {
+    const res = await axios.post("http://localhost:8000/auth/jwt/create", {
+      username,
+      password,
+    });
+    setToken(res.data.access);
+  };
+
+  const handleUsernameChange = (event: { target: any }) => {
+    // FIXME any
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: { target: any }) => {
+    // FIXME any
+    setPassword(event.target.value);
+  };
 
   return (
     <>
-      <Header name={reader?.name} loggedIn={reader ? true : false} />
+      <Header name={reader?.name} loggedIn={reader.name ? true : false} />
       <main>
         <h1>Library Quest</h1>
+        <form>
+          <input name="username" onInput={handleUsernameChange}></input>
+          <input name="password" onInput={handlePasswordChange}></input>
+          <button type="button" onClick={handleLogin}>
+            Submit
+          </button>
+        </form>
         <MapBox libraries={libraries} reader={reader} />
       </main>
     </>

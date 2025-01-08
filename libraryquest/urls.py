b4edc1d36ce387b435a93e398d007ec31776a10a
dovekie <dovekie@gmail.com>
2025-01-08 12:06:03 -0800

    use openAPI spec generator

--- libraryquest/urls.py
+++ libraryquest/urls.py
@@ -15,6 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -31,4 +37,21 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
+
+    # OpenAPI
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    
+    # Two different OpenAPI UIs. Don't need both. FIXME pick one
+    # Swagger UI:
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    # Redoc UI:
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
